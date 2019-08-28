a8ba8150464effd0f330dc8707dcce254cdef3ba
marb <marb61a@gmail.com>
2019-08-28 16:47:05 +0100

    updtaed title and rating scraping function and add initial poster scraping function

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const request = require('request-promise');
 const cheerio = require('cheerio');
 
 const sampleResult = {
-    title = 'Bohemian Rhapsody',
+    title: 'Bohemian Rhapsody',
     rank: 1,
     imdbRating: 8.0,
     descriptionUrl: 'https://www.imdb.com/title/tt1727824/?ref_=nv_sr_1?ref_=nv_sr_1',
@@ -16,7 +16,31 @@ async function scrapingTitlesRankingsAndRatings(){
     );
 
     const $ = await cheerio.load(result);
-    
+
+    // Using the parent element tr as you would not be able to get the rating
+    // from inside the td title elementelement).find(
+    const movies = $('tr')
+        .map((i, element) => {
+            const title = $(element).find('td.titleColumn > a').text();
+            const descriptionUrl = 
+            'https://www.imdb.com/' + $(element).find('td.titleColumn > a').attr('href');
+            const imdbRating = $(element).find('td.ratingColumn.imdbRating').text().trim();
+
+            // The ranking can be resolved easily by attaching it to the index
+            // which is created when mapping
+            return {title, imdbRating, rank: i, descriptionUrl}; 
+        })
+        .get();
+
+    console.log(movies);
+}
+
+async function scrapePosterUrl(movies){
+    const moviePosterUrls = await Promise.all(
+        movies.map(async movie => {
+
+        })
+    );
 }
 
 scrapingTitlesRankingsAndRatings();
